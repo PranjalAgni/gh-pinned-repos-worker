0d877ceb01739310ad4dcd062c0978cc9754243e
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-06-30 02:23:37 +0530

    :sparkles: worker code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+const GH_TOKEN = "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";
+const GH_API_URL = "https://api.github.com/graphql";
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
@@ -7,6 +9,25 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+
+interface GithubPinnedRepos {
+  data: {
+    user: {
+      pinnedItems: {
+        nodes: {
+          name: string;
+          url: string;
+          description: string;
+          languages: {
+            nodes: {
+              name: string;
+            }[];
+          };
+        }[];
+      };
+    };
+  };
+}
 
 export interface Env {
   // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
@@ -19,12 +40,87 @@ export interface Env {
   // MY_BUCKET: R2Bucket;
 }
 
+const getGithubUsername = (request: Request) => {
+  const url = new URL(request.url);
+  const username = url.searchParams.get("username");
+  return username;
+};
+
+const isResponseValid = (response: GithubPinnedRepos) => {
+  return response.data.user !== null;
+};
+
+const convertResponseToPinnedRepositoryList = (response: GithubPinnedRepos) => {
+  const { pinnedItems } = response?.data?.user;
+  const pinnedRepos = pinnedItems.nodes.map((node, idx) => {
+    return {
+      id: String(idx),
+      repo: node.name,
+      url: node.url,
+      description: node.description,
+      language: node.languages.nodes.map((languageNode) => languageNode.name),
+    };
+  });
+
+  return pinnedRepos;
+};
+
+const getPinnedRepos = async (username: string) => {
+  const query = `
+  query { 
+    user (login: "${username}") {
+      pinnedItems(first: 10, types: REPOSITORY) {
+        nodes {
+          ... on Repository {
+            name
+            url
+            description
+            languages(first: 5) {
+              nodes {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }`;
+
+  const response = await fetch(GH_API_URL, {
+    method: "POST",
+    body: JSON.stringify({ query }),
+    headers: {
+      Authorization: `Bearer ${GH_TOKEN}`,
+      "User-Agent": "Github Cloudflare worker",
+    },
+  });
+
+  const body: GithubPinnedRepos = await response.json();
+  const isValid = isResponseValid(body);
+  if (!isValid) return [];
+  return convertResponseToPinnedRepositoryList(body);
+};
+
 export default {
   async fetch(
     request: Request,
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return new Response("Hello World!");
+    const username = getGithubUsername(request);
+    if (!username) {
+      return new Response(
+        JSON.stringify({
+          error: true,
+          message:
+            "Username not found, example GET request https://gh-pinned-repos-worker.pranjal.workers.dev?username=${GH_USER_NAME}",
+        }),
+        {
+          status: 500,
+        }
+      );
+    }
+    const pinnedRepos = await getPinnedRepos(username);
+    return new Response(JSON.stringify(pinnedRepos, null, 3));
   },
 };
